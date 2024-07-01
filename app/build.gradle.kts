ccaf5b48a831734ba0a4548e862fd66918ab9985
AlexMac <kolibelrazuma@gmail.com>
2024-07-01 04:50:16 +0300

    Project started

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    //ViewModel
+    implementation(libs.lifecycle.viewmodel.ktx)
 }
